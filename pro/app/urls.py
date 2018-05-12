663d135c21af3895c464d81365927c2b40477df1
Mano Sriram <mano.sriram0@gmail.com>
2018-05-13 00:22:41 +0530

    Added a main Django Project Folder

--- pro/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('another/',views.another,name="another"),
-    path('home/',views.index,name = "index"),
-]
-
-app_name = 'app'
--- pro/app/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def index(request):
-    return render(request,'app/index.html')
-
-def another(request):
-    return render(request,'app/another.html')
-    
