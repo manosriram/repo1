45b5e44371f9de18bafb421a6403cef5f6cae6ef
Mano Sriram <mano.sriram0@gmail.com>
2018-05-30 00:20:40 +0530

    Removed Unnecessary files

--- pro/app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class profile(models.Model):
-    user = models.OneToOneField(User,on_delete=models.CASCADE)
-    site = models.URLField()
-    profile_pic = models.ImageField(upload_to='profile_pic',blank=True)
-
-
--- pro/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from app import views
-
-
-urlpatterns = [
-    path('register/',views.register,name="register"),
-]
-
-app_name = 'app'
