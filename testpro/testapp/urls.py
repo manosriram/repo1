4cc47499d558229aa697ef01455c4d93ccb26562
Mano Sriram <mano.sriram0@gmail.com>
2018-05-29 22:15:46 +0530

    Added User Authentication Feature...

--- testpro/testapp/urls.py
+++ testpro/testapp/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [
     path('register/',views.register,name="register"),
     path('contact/',views.contact,name="contact"),
+    path('user_login/',views.user_login,name="user_login"),
+    path('info/',views.data_info,name = "info"),
 ]
 
 app_name = 'testapp'
--- testpro/testpro/urls.py
+++ testpro/testpro/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     path('',views.index,name = "index"),
     path('admin/', admin.site.urls),
     path('testapp/',include('testapp.urls')),
+    path('logout/',views.user_logout,name="logout"),
+    path('special/',views.special,name = "special"),
+
 ]
