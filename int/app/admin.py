6159152010d622a99b23d34d25a1963603aba586
Mano Sriram <mano.sriram0@gmail.com>
2018-06-10 00:11:04 +0530

    1

--- int/app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import comments_feed,buy_model
-
-admin.site.register(comments_feed)
-admin.site.register(buy_model)
--- int/procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn pro.wsgi
