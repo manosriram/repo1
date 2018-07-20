aac0e94ba03527a48e05eb471d4174b5adf1140c
Mano Sriram <mano.sriram0@gmail.com>
2018-07-20 23:16:54 +0530

    MockVita files..

---
+++ competitive_prog/mockVita/test.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+
+using namespace std;
+
+
+// static int c=0;
+int binary(int n) {
+    int t;
+    int cnt=0,b[40];
+    int count=0;
+    while(n>=2) {
+        b[cnt] = n%2;
+        n=n/2;
+        cnt++;
+    }
+
+    for (t=0;t<cnt;t++) {
+        if (b[t]==1) {
+        count++;
+        }
+
+        else {
+        continue;
+        }
+    }
+    
+    return count+1;
+}
+
+
+
+
+int main() {
+    int temp1[40],a[40],n1,i,j,res=0;
+    cin >> n1;
+
+    for (i=0;i<n1;i++)
+    cin >> a[i];
+
+    for (i=0;i<n1;i++) {
+    temp1[i] = binary(a[i]);
+    }
+    
+    
+    for (i=0;i<n1;i++) {
+        for (j=0;j<n1;j++) {
+            if (temp1[i]>temp1[j] && i<j) {
+            res++;
+            }
+            else
+            continue;
+        }
+    }
+
+
+    // for (i=0;i<n1;i++)
+    // cout << temp1[i] << endl;
+ cout << res;
+}
