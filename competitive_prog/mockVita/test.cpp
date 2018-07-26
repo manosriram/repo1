bb987baaafe2c5fbef9643d4c29d52810f40a7ef
Mano Sriram <mano.sriram0@gmail.com>
2018-07-26 12:21:31 +0530

    updated file.

--- competitive_prog/mockVita/test.cpp
+++ competitive_prog/mockVita/test.cpp
@@ -13,35 +13,28 @@ int binary(int n) {
         n=n/2;
         cnt++;
     }
-
     for (t=0;t<cnt;t++) {
         if (b[t]==1) {
         count++;
         }
-
         else {
         continue;
         }
-    }
-    
+    }   
     return count+1;
 }
-
-
-
-
 int main() {
     int temp1[40],a[40],n1,i,j,res=0;
     cin >> n1;
+    
+    if (n1<=50) {
 
-    for (i=0;i<n1;i++)
+    for (i=0;i<n1 && i<10000000;i++)
     cin >> a[i];
 
     for (i=0;i<n1;i++) {
     temp1[i] = binary(a[i]);
     }
-    
-    
     for (i=0;i<n1;i++) {
         for (j=0;j<n1;j++) {
             if (temp1[i]>temp1[j] && i<j) {
@@ -51,9 +44,6 @@ int main() {
             continue;
         }
     }
-
-
-    // for (i=0;i<n1;i++)
-    // cout << temp1[i] << endl;
  cout << res;
+}
 }
