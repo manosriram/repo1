0fe74849977bdd7aeb4b9eff3863a770dfa60584
Mano Sriram <mano.sriram0@gmail.com>
2018-08-01 01:48:46 +0530

    encryption added

--- competitive_prog/c++/tester.cpp
+++ competitive_prog/c++/tester.cpp
@@ -3,11 +3,12 @@
 
 using namespace std;
 int counted=0;
+int cnt=0;
 int a[100],t,n,check,a1[100],m,n1,temp;
 int track,out=0,out1=0;
 static int rank1[100];
 int record[100];
-
+int b[100];
 
 int rankCounter(int a[100]) {
 int count;
@@ -25,73 +26,52 @@ int count;
     return rank1[t];
 }
 
-int addAlice(int a[100],int a1[100],int track,int n,int out,int out1) {
-
-for (t=0;t<n+1;t++) {
-    if (a[t]==track) {
-        record[out1] = rank1[t];
-        out++;
-        out1++;         
-    }
-    else
-    continue;
-}
-return record[out1];
-}
-
-
-
-int removeAlice(int a[100],int track,int n) {
-    for(int i=0; i<n+1; i++)
-	{
-		if(a[i]==track)
-		{
-			for(int j=i; j<(n-2); j++)
-			{
-				a[j+1]=a[j+2];
-			}
-			counted++;
-			break;
-		}
-	}
-    return a[n+1];
-}
-
-
-
 int sortIt(int a[100]) {
-    sort(a, a+n+1, greater<int>());
-    return a[n+1];
+    std::sort(a,a+n+n1,std::greater<int>());
+    return a[n+n1+1];
 }
+
+
+int returnRank(int a[100],int b[100]) {
+    for (t=0;t<n1;t++) {
+        if (a[t]!=b[t])
+        cnt++;
+
+        if (a[t]==b[t])
+        return rank1[cnt-1];
+        
+    }
+
+}
+
+
 
 int main() {
-    
+    int getRank;
+    int c[100],cntag=0;
     cin>>n;
     
     for (t=0;t<n;t++)
     cin>>a[t];
 
-
-    cin >> n1;
-    cout << "Enter Alice's Scores : " << endl;
-//Alice Scores...
-    for (t=0;t<n1;t++)
-    cin >> a1[t];
-   
-
     rankCounter(a);
+      
+    cin >> n1;
 
-    for (t=0;t<n;t++)
-    cout<<"Rank of Value " << " " << a[t] << " is " << rank1[t] << endl;
-    
+    for (t=0;t<n1;t++)
+    cin >> b[t];
+
     for (t=0;t<n1;t++) {
-        a[n+2+t] = a1[t];
+        a[n] = b[t];
         sortIt(a);
         rankCounter(a);
-        addAlice(a,a1,track,n,out,out1);
-        removeAlice(a,track,n);
-    }
-for (t=0;t<n;t++)
-cout << record[t];
+        getRank = returnRank(a,b);
+        c[cntag] = getRank;
+        cntag++;
     }
 
+     
+    for (t=0;t<n+n1;t++)
+    cout<<"Rank of Value " << " " << a[t] << " is " << rank1[t] << endl; 
+    
+    }
