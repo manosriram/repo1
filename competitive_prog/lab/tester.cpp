7646f7ba2f7164f52ab8e95829d4c8c10b20f9fc
Mano Sriram <mano.sriram0@gmail.com>
2018-08-10 19:24:18 +0530

    case study..

---
+++ competitive_prog/lab/tester.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int day,month,year;
+    char a,b;
+
+    
+
+    cout << day << "/" << month << "/" << year;
+}
--- competitive_prog/mockVita/stringRot.cpp
+++ competitive_prog/mockVita/stringRot.cpp
@@ -4,38 +4,16 @@ using namespace std;
 
 int t,n,len=0;
 
-char lock[30];
-static int cnt;
+
+
+int cnt1=0;
 string s;
 char temp;
 
-char leftRotate(char a[100],int a1[100]) {
-
-  for (t=0;t<a[t];t++) {
-      temp = s[0];
-
-      for (int i=0;i<n-1;i++) {
-          s[i] = s[i+1];
-      }
-      s[n-1] = temp;
-  }
-  return s[0];
-}
-
-char rightRotate(char a[100],int a1[100]) {
-  for (t=0;t<a[t];t++) {
-      temp = s[len-1];
-
-      for (int i=len-1;i>=0;i--) {
-          s[i+1] = s[i];
-      }
-      s[0] = temp;
-}
-return s[0];
-}
-
 
 int main() {
+  static int cnt;
+  static string lock[30];
   char a[100];
   int a1[100];
   cin >> s;
@@ -49,17 +27,39 @@ cin >> n;
 
   for (t=0;t<n;t++) {
     if (a[t]=='L') {
-      lock[cnt] = leftRotate(a,a1);
-      cnt++;
+
+      for (t=0;t<a1[t];t++) {
+      temp = s[0];
+
+      for (int i=0;i<n-1;i++) {
+          s[i] = s[i+1];
+      }
+      s[n-1] = temp;
+
+  }
+  lock[cnt] = s[0];
+  cnt++;
     }
+
+
     if (a[t]=='R') {
-      lock[cnt] = rightRotate(a,a1);
-      cnt++;
+
+      for (t=0;t<a1[t];t++) {
+      temp = s[len-1];
+
+      for (int i=len-1;i>=0;i--) {
+          s[i+1] = s[i];
+      }
+      s[0] = temp;
     }
+    lock[cnt] = s[0];
+    cnt++;
   }
 
-  for (t=0;t<cnt;t++)
-  cout << lock[t];
 
+}
+
+for (t=0;t<cnt;t++)
+cout << lock[t];
 
 }
