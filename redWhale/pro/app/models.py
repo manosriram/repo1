c3de2d8b6c5fdf91f586480283ca4b9ee2e0aa09
Mano Sriram <mano.sriram0@gmail.com>
2018-09-17 22:44:39 +0530

    added redWhale task files..

--- redWhale/pro/app/models.py
+++ redWhale/pro/app/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 
 class profileUser(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profilePic = models.ImageField(upload_to='profile_pics', blank=True)
+    # profilePic = models.ImageField(upload_to='profile_pics', blank=True)
